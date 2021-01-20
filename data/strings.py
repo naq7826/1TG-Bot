9e207565208de7b52a4b33a4f23daf842b72f954
naq7826 <47947675+naq7826@users.noreply.github.com>
2021-01-21 05:26:08 +0700

    Updated code

---
+++ data/strings.py
@@ -0,0 +1,4 @@
+shareTitle = "Share account Valorant"
+shareDesc = "Hỏi kỹ chủ acc trước khi thay đổi settings! Rank có thể thay đổi so với hiện tại!"
+missingArgs = "Kiểm tra lại các thông tin!"
+announceTitle = "THÔNG BÁO, THÔNG BÁO, ALO ALO ALOOO"
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 import os
+import asyncio
+from data.strings import *
 from discord.ext import commands
 from dotenv import load_dotenv
 load_dotenv(dotenv_path='.env')
@@ -10,13 +12,17 @@ client = commands.Bot(command_prefix='x!')
 @client.event
 async def on_ready():
 	print('We have logged in as {0.user}'.format(client))
-	await client.change_presence(activity=discord.Game('cái loz'))
+	await client.change_presence(activity=discord.Game('made by Quanimus'))
     
 
 @client.command()
-async def share(ctx, usr, pw, rank0, rank1="", rank2=""):
+async def share(ctx, usr="", pw="", rank0="", rank1="", rank2=""):
+	if usr == "" or pw == "" or rank0 == "":
+		await ctx.send(missingArgs, delete_after=3)
+		await ctx.message.delete()
+		return
 	rank = [rank0, rank1, rank2]
-	embed = discord.Embed(title="Share account Valorant", description="Hỏi kỹ chủ acc trước khi thay đổi settings! Rank có thể thay đổi so với hiện tại!", color=0xff5252)
+	embed = discord.Embed(title=shareTitle, description=shareDesc, color=0xff5252)
 	embed.add_field(name="Username", value=usr, inline=True)
 	embed.add_field(name="Password", value=pw, inline=True)
 	embed.add_field(name="Rank", value=" ".join(rank), inline=True)
@@ -25,7 +31,7 @@ async def share(ctx, usr, pw, rank0, rank1="", rank2=""):
 
 @client.command()
 async def announce(ctx, mess):
-  embed = discord.Embed(title="THÔNG BÁO, THÔNG BÁO, ALO ALO ALOOO", description=mess, color=0xff5252)
+  embed = discord.Embed(title=announceTitle, description=mess, color=0xff5252)
   await ctx.send(embed=embed)
   await ctx.message.delete()
 
